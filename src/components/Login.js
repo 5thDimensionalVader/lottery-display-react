25ac6dc15c690a3ecd613eaa1841a9f8a64bd117
5thDimensionalVader <5thDimensionalVader@protonmail.com>
2021-07-11 14:00:22 +0000

    lottery-display-react Logic component completed

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,53 @@
 import './login.css';
+import { HiOutlineTicket } from 'react-icons/hi';
 
 const Login = () => {
   return (
     <div className="login">
-      <h3>Login Page</h3>
+      {/* LEFT PANEL */}
+      <div className="leftPanel">
+
+        <div className="leftPanelText">
+          <span>Make it simple, but<br />significant Lottery Display</span>
+        </div>
+      </div>
+
+      {/* RIGHT PANEL */}
+      <div className="rightPanel">
+
+        <div className="rightPanelLogo">
+          <span><HiOutlineTicket size={40} color="#09166a" /></span>
+          <span>Lottery Display</span>
+        </div>
+
+        <div className="rightPanelLoginContainer">
+          <span>Login</span>
+          <span>Login to your account</span>
+          <span>Thank you for get back to Lottery Display, lets access our the<br /> best recommendation for you</span>
+          <span>
+            <label>Username</label>
+            <input type="text" />
+          </span>
+
+          <span>
+            <label>Password</label>
+            <input type="text" />
+          </span>
+
+          <div className="rightPanelReset">
+            <div>
+              <input type="checkbox" />
+              <label>Remember me</label>
+            </div>
+            <label>Reset Password?</label>
+          </div>
+
+          <span><button>Sign In</button></span>
+          <span>Don't have an account yet? Join Lottery Display</span>
+        </div>
+      </div>
     </div>
   );
 }
- 
+
 export default Login;
